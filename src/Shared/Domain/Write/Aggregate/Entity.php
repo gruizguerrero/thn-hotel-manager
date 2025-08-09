de1000356415bdfca600654d694358399af5235d
gonzalo.ruiz <gonzaloruizguerrero@gmail.com>
2025-08-09 19:04:25 +0200

    add Uuid property and methods to Entity class

--- src/Shared/Domain/Write/Aggregate/Entity.php
+++ src/Shared/Domain/Write/Aggregate/Entity.php
@@ -2,7 +2,24 @@
 
 namespace App\Shared\Domain\Write\Aggregate;
 
+use App\Shared\Domain\ValueObject\Uuid;
+
 abstract class Entity
 {
+    protected Uuid $id;
 
+    protected function __construct(Uuid $id)
+    {
+        $this->setId($id);
+    }
+
+    private function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function id(): Uuid
+    {
+        return $this->id;
+    }
 }
