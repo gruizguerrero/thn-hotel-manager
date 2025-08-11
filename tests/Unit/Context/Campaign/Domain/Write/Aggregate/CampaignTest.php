cdff47b11cc1aab46d1b1dff9ca4d4d1686be791
gonzalo.ruiz <gonzaloruizguerrero@gmail.com>
2025-08-11 18:20:52 +0200

    refactor CampaignTest to use Name value object for campaign name

--- tests/Unit/Context/Campaign/Domain/Write/Aggregate/CampaignTest.php
+++ tests/Unit/Context/Campaign/Domain/Write/Aggregate/CampaignTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Context\Campaign\Domain\Write\Aggregate;
 
 use App\Context\Campaign\Domain\Write\Aggregate\Campaign;
+use App\Context\Campaign\Domain\Write\Aggregate\ValueObject\Name;
 use App\Shared\Domain\Service\UuidGenerator;
 use PHPUnit\Framework\TestCase;
 
@@ -13,11 +14,11 @@ final class CampaignTest extends TestCase
     public function test_campaign_is_created(): void
     {
         $id = UuidGenerator::generate();
-        $name = 'Test Campaign';
+        $name = Name::fromString('test');
 
         $campaign = Campaign::create($id, $name);
 
         $this->assertTrue($id->equalsTo($campaign->id()));
-        $this->assertEquals($name, $campaign->name());
+        $this->assertEquals($name->value(), $campaign->name()->value());
     }
 }
